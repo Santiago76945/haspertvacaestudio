8c094e4a4abfe443d23998308442591706fd2428
Santiago76945 <santiago_76945@yahoo.es>
2025-03-18 15:15:10 -0300

    sent message configurated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,16 @@
 // src/pages/Contact.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/contact.css';
 
 const Contact = () => {
-  
+  const [submitted, setSubmitted] = useState(false);
+
   const handleSubmit = (event) => {
-    event.preventDefault(); // Evita que el formulario recargue la página
-    
-    // Muestra el mensaje de confirmación
-    alert("¡Gracias por contactarnos! Nuestro equipo se pondrá en contacto a la brevedad.");
-    
-    // (Opcional) Restablecer el formulario después del envío
+    event.preventDefault();
+    setSubmitted(true);
     event.target.reset();
   };
 
@@ -22,18 +19,31 @@ const Contact = () => {
       <Header />
       <section className="contact">
         <h2>Contacto</h2>
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Nombre:</label>
-          <input type="text" id="name" name="name" required />
-          
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-          
-          <label htmlFor="message">Mensaje:</label>
-          <textarea id="message" name="message" rows="5" required></textarea>
-          
-          <button type="submit" className="contact-button">Enviar Mensaje</button>
-        </form>
+        {submitted ? (
+          <div className="contact-confirmation">
+            <img
+              src="/images/message-sent.png" // Asegúrate de colocar la imagen en la ruta correcta
+              alt="Mensaje Enviado"
+              className="confirmation-image"
+            />
+            <p>
+              Muchas gracias por comunicarse con nosotros, nos pondremos en contacto con usted a la brevedad.
+            </p>
+          </div>
+        ) : (
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Nombre:</label>
+            <input type="text" id="name" name="name" required />
+            
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+            
+            <label htmlFor="message">Mensaje:</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+            
+            <button type="submit" className="contact-button">Enviar Mensaje</button>
+          </form>
+        )}
       </section>
       <Footer />
     </div>
