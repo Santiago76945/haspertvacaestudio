b2fa6818450b2a68d74c7e25799f7bb3ab39d3df
Santiago76945 <santiago_76945@yahoo.es>
2025-05-01 00:18:00 -0300

    horarios updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     address: "Dirección: Calle Chacabuco N° 150, Oficinas 11 y 12 “B”, Centro, Córdoba Capital",
     phone: "Teléfono: +54 9 351 743 2323",
     email: "Email: estudioVGH@yahoo.com",
-    schedule: "Horario de Atención: Lunes a Viernes de 10:00 a 17:00",
+    schedule: "Horario de Atención: Lunes a jueves de 9:00 a 18:00, Viernes de 9:00 a 14:00, Sábados y domingos cerrado",
     rights: "Todos los derechos reservados."
   };
 
@@ -24,7 +24,7 @@ const Footer = () => {
     address: "Address: Chacabuco Street 150, Offices 11 and 12 “B”, Downtown, Córdoba Capital, Argentina",
     phone: "Phone: +54 9 351 743 2323",
     email: "Email: estudioVGH@yahoo.com",
-    schedule: "Office Hours: Monday to Friday from 10:00 to 17:00",
+    schedule: "Office Hours: Monday to Thursday from 9:00 to 18:00, Friday from 9:00 to 14:00, Saturday and Sunday closed",
     rights: "All rights reserved."
   };
 
@@ -50,4 +50,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
